231501b9e2049da169ab92e7b91283a1e0b4c0e1
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2022-05-03 15:28:59 +0900

    Feat: display items

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,61 @@
+import { useState } from "react";
+import { useQuery } from "react-query";
+
+//Components
+import Items from "./Item/Items";
+import Drawer from "@material-ui/core/Drawer";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import Grid from "@material-ui/core/Grid";
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
+import Badge from "@material-ui/core/Badge";
+
+//Styles
+import { Wrapper } from "./App.styles";
+
+//Types
+
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
+
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch("https://fakestoreapi.com/products")).json();
+
 const App = () => {
-  return <div className="App">Start</div>;
+  const [cartOpen, setCartOpen] = useState(false);
+
+  const [cartItem, setCarItem] = useState<CartItemType[]>([]);
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    "products",
+    getProducts
+  );
+
+  const getTotalItems = () => null;
+
+  const handleAddToCart = (clickedItem: CartItemType) => null;
+
+  const handleDeleteFromCart = () => null;
+
+  if (isLoading) return <LinearProgress />;
+  if (error) return <div>에러 발생</div>;
+
+  return (
+    <Wrapper>
+      <Grid container spacing={3}>
+        {data?.map((item) => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Items item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
+  );
 };
 
 export default App;
